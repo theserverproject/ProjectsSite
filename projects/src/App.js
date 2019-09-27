a6d4c7d26992d19786c4f87a201062dffb5854a9
Ross Alexandra <rossandus@hotmail.com>
2019-09-27 09:57:29 -0700

    Changed default react page to blank app.

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navbar from './pages/home/components/navbar/navbar';
+import Main from './pages/home/components/main/main';
+import Footer from './pages/home/components/footer/footer';
+
+// Import css classes
 import './App.css';
+import './AppVars.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to the projects page!
-        </p>
-        <p>
-          Please check back regularly to see if this is setup
-        </p>
-      </header>
+    <div className="page-content">
+        <Navbar/>
+        <Main/>
+        <Footer/>
     </div>
   );
 }
