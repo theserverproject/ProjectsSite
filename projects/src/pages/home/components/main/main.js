ccb355b55b4d825ec68c34c24b92a4d663e600b3
Ross Alexandra <rossandus@hotmail.com>
2019-10-07 13:23:20 -0700

    GH-6 - Added base css and js for panels.

--- projects/src/pages/home/components/main/main.js
+++ projects/src/pages/home/components/main/main.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './main.css';
 
+import Panel from '../../../../basicComponents/panel/Panel'
+
 const Main = () => {
 
     return (
         <main id="main">
-
+            <div className="content-seperator"/>
+            <Panel classOverride="project-panel"/>
+            <div className="content-seperator"/>
+            <Panel classOverride="project-panel"/>
+            <div className="content-seperator"/>
+            <Panel classOverride="project-panel"/>
+            <div className="content-seperator"/>
         </main>
     );
 
