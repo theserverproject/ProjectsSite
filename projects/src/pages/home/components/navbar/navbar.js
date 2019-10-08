6a1fb414ab00e971cd1b8854f92fd99c543fdbfa
Ross Alexandra <rossandus@hotmail.com>
2019-10-08 10:10:13 -0700

    GH-5 - Added base navbar stuff. Dropdown currently non-functioning.

--- projects/src/pages/home/components/navbar/navbar.js
+++ projects/src/pages/home/components/navbar/navbar.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './navbar.css';
 
+import Image from '../../../../basicComponents/image/Image';
+import Dropdown from '../../../../basicComponents/dropdown/Dropdown';
+
 const Navbar = () => {
+
+    let dropdownItems = [];
+
+    
 
     return (
         <header id="navbar">
-
+            <div className='logo-box'>
+                <Image classOverride={ 'logo'  } image='logo.png' imageLink='http://www.theserverproject.com' defaultText="Logo image not found."/>
+            </div>
+            <div className='title-box'>
+                <h1 className='title-text'>The Projects</h1>
+            </div>
+            <div className='dropdown-box'>
+                <Dropdown dropdownItems={ dropdownItems } classOverride='dropdown' dropdownTitleClassOverride='dropdown-text'>Projects List</Dropdown>
+            </div>
         </header>
     );
 
