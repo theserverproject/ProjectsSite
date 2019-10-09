6058264de9f6989f8bd79ecf7b44431a08b1a5e1
Ross Alexandra <rossandus@hotmail.com>
2019-10-09 09:51:47 -0700

    GH-5 - Added logo, and made dropdown work.

--- projects/src/pages/home/components/navbar/navbar.js
+++ projects/src/pages/home/components/navbar/navbar.js
@@ -4,11 +4,13 @@ import './navbar.css';
 import Image from '../../../../basicComponents/image/Image';
 import Dropdown from '../../../../basicComponents/dropdown/Dropdown';
 
-const Navbar = () => {
+const Navbar = (props) => {
 
     let dropdownItems = [];
 
-    
+    props.projects.forEach((project) => {
+        dropdownItems.push(<a key={ project.shortName } href={ project.serverLink } className="dropdown-item">{ project.shortName }</a>);
+    });
 
     return (
         <header id="navbar">
